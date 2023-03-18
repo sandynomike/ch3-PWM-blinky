b55a7236ae0a48200f24f7fef0b6c87a23ffa32c
Mike <sandynomike@gmail.com>
2023-03-19 08:19:08 +0900

    Updated comments in main.c

--- main.c
+++ main.c
@@ -13,16 +13,30 @@
 //  The PWM output will use channel 2 of timer 1 (TIM2_CH1) which is output on pin A0.
 //  The LED will be attached to this A0 pin. Since the pin will be driven by a PWM timer,
 //  this is considered an "alternate function" (AF) of the GPIO pin, and must be set as
-//  such in the GPIOA CNF bits.
+//  such in the GPIOA CNF/MODE bits:
 //
-//  Using the internal 8 MHz clock, in order to achieve a 1 Hz blink rate with a 50%
+//      Alternate Function, Push-Pull Output, 2 MHz max speed setting:
+//      CNF0[1:0] = 0b10, MODE0[1:0] = 0b10
+//
+//  Using the internal 8 MHz clock, in order to achieve a 1 Hz blink rate with a 50% duty
 //  cycle, the period needs to be 8E6 with the capture/compare setting of 4E6. However,
-//  since we are using a 16-bit register, with a maximum value of 65,535, the prescaler
-//  must also be used. So, set the period to be 8000 and the capture/compare register to
-//  4000. This will be 1000 times too fast, so we set the prescaler to 1000 (using a
-// setting of 999 since a prescaler setting of 0 is actually divide-by-1.) This will
-//  result in a period of 8000 x 1000 (8E6 pulses) with a pin state change at 4000 x
-//  1000 (4E6) for a 50% duty cycle.
+//  since we are using a 16-bit register, the maximum settable period is 65,535 ticks at
+//  a frequency of 8 MHz.
+//
+//       Period = 65,535 ticks / 8 MHz = 8.19 ms
+//    Frequency = 1 / 8.19 ms = 122 Hz
+//
+//  An LED flashing at 122 Hz is too fast to see. So we need to use a prescaler to slow
+//  down the PWM clock. Using a prescaler of 8000:
+//
+//      8 MHz main clock / 8000 prescaler = 1 kHz PWM clock
+//
+//  With this, the PWM clock period can easily be set in miliseconds in the TIM2 ARR register and
+//  likewise, the duty cycle period in the TIM2 CCR1 register. Nice! For a 1 Hz clock with a 50%
+//  duty cycle:
+//
+//      TIM2->ARR = 1000  (1000 ms PWM period)
+//      TIM2->CCR1 = 500  (duty cycle = 500 / 1000 = 50%)
 
 
 //  Software Setup:
@@ -33,13 +47,13 @@
 //     0b10, and MODE0[1:0] bits as 0b10. Remember that the default setting for
 //     CNF0[1:0] is 0b01, so that CNF0[0] bit must be specifically cleared!
 //  3. Enable TIM 2 by setting the RCC_APB1ENR_TIM2EN bit in the RCC APB1ENR register.
-//  4. Set PWM period via the TIM2 ARR register.
-//  5. Set PWM duty cycle via the TIM2 CCR1 register.
-//  6. Set PWM mode 1 by setting the TIM_CCMR1_OC1Mx bits as 110 via the TIM2 CCMR1
+//  4. Set the prescaler via the TIM2 PSC register to slow down the output frequency.
+//  5. Set PWM period via the TIM2 ARR register.
+//  6. Set PWM duty cycle via the TIM2 CCR1 register.
+//  7. Set PWM mode 1 by setting the TIM_CCMR1_OC1Mx bits as 110 via the TIM2 CCMR1
 //     register.
-//  7. Enable output of TIM2 Channel 1 to pin A0 by setting the TIM_CCER_CC1E bit in
+//  8. Enable output of TIM2 Channel 1 to pin A0 by setting the TIM_CCER_CC1E bit in
 //     the TIM2 CCER register.
-//  8. Set the prescaler via the TIM2 PSC register to slow down the output frequency.
 //  9. Enable (turn on) the clock counter via the TIM_CR1_CEN bit in the TIM2 CR1
 //     register.
 
@@ -61,14 +75,14 @@ main( void )
     //  3. Enable TIM 2 by setting the RCC_APB1ENR_TIM2EN bit in the RCC APB1ENR register.
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
 
-    //  4. Set PWM period via the TIM2 ARR register.
-    TIM2->ARR = 8000;
+    //  4. Set the prescaler to divide the PWM clock by 8000 via the TIM2 PSC register.
+    TIM2->PSC = 8000;
 
-    //  5. Set PWM duty cycle via the TIM2 CCR1 register.
-    TIM2->CCR1 = 4000;
+    //  5. Set PWM period via the TIM2 ARR register.
+    TIM2->ARR = 1000;
 
-    //  6. Set the prescaler to divide the PWM clock by 1000 via the TIM2 PSC register.
-    TIM2->PSC = 1000;
+    //  6. Set PWM duty cycle via the TIM2 CCR1 register.
+    TIM2->CCR1 = 500;
 
     //  7. Set PWM mode 1 by setting the TIM_CCMR1_OC1M_x bits as 110 via the TIM2 CCMR1
     //     register.
